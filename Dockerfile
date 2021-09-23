266558f93f5e6bcbaa9fd2b39df5e0545b7414aa
Jiham Kim <jzh04082579@gmail.com>
2021-09-23 07:09:47 +0000

    도커 소스파일 변경

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,3 @@
 FROM node:6 
 EXPOSE 3000 
-COPY index.js . 
-CMD node index.js
-
+CMD node /www/index.js
--- index.js
+++
@@ -1,6 +0,0 @@
-var http = require('http'); 
-var handleRequest = function(req, res){
-	res.end('hello');
-}; 
-var www = http.createServer(handleRequest);
-www.listen(3000);
